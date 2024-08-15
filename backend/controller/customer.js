a90b210b1fda7264c37fbe0b24ec88f50fe3fcd7
EarthY.Y <supanat5947@gmail.com>
2024-08-15 15:42:30 +0700

    วางโครงสร้าง without vite

--- backend/controller/customer.js
+++ backend/controller/customer.js
@@ -31,7 +31,8 @@ export const getCustomerById = async (req, res) => {
 
 export const createCustomer = async (req, res) => {
     const {phone_number, username, password, confPassword} = req.body;
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    
+    if(password !== confPassword) return res.status(400).json({msg: "Password not match"});
     //เข้ารหัส password กันโดนโจมตีระหว่างทาง
     const hashPassword = await argon2.hash(password);
     try {
@@ -40,7 +41,7 @@ export const createCustomer = async (req, res) => {
             phone_number: phone_number,
             password: hashPassword,
         });
-        res.status(201).json({msg: "Register Berhasil"});
+        res.status(201).json({msg: "Register complete"});
     } catch (error) {
         res.status(400).json({msg: error.message});
     }
