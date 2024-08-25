1da1a61a7952ddb72740d570826a18d286ce96a8
kieee1246 <kieee1246@gmail.com>
2024-08-25 14:25:56 +0700

    ตัวอย่าง Login
    ตัวอย่าง Login

---
+++ frontend/components/login-page/login-page.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title text-center">เข้าสู่ระบบ</h3>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">อีเมล</label>
+                  <input type="email" className="form-control" id="email" placeholder="อีเมล หรือ เบอร์โทรศัพท์" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">รหัสผ่าน</label>
+                  <input type="password" className="form-control" id="password" placeholder="รหัสผ่าน" />
+                  <label ></label>
+                </div>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">เข้าสู่ระบบ</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '../public/vite.svg'
 import { BrowserRouter, Routes,  Route} from 'react-router-dom'
 import Dashboard from '../components/admin/dashboard'
+import Login from '../components/login-page/login-page'
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login-page" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
