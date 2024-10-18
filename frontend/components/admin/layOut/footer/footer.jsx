14256bc7cd685061f137273811846f42bc45d62f
kieee1246 <kieee1246@gmail.com>
2024-10-18 10:50:17 +0700

    EditFooter
    แก้ไข คอนเทน เนื้อหา จากตรงกลางไปด้านซ้าย

--- frontend/components/admin/layOut/footer/footer.jsx
+++ frontend/components/admin/layOut/footer/footer.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Footer = () => {
   return (
     <footer className="text-white py-5 d-flex align-items-end flex-column" style={{ backgroundColor: '#C40C0C'}}>
-      <div className="container text-center mt-auto p-2">
+      <div className="container col-12 text-start mt-auto p-2 ">
         <h5>About</h5>
         <p>ร้านโฮมเมดเบเกอร์รี่แอนด์คุกกี้ เป็นร้านขายขนมที่ผลิตสดใหม่ทุกวัน</p>
         <h5>Contact</h5>
