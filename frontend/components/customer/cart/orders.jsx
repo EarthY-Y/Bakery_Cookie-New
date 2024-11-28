fe44b7ac6d74ae8ea2bd04716a4265c7dd614ae4
IMcang <imboon.patcharawut@gmail.com>
2024-11-28 00:13:34 -0800

    แก้ขนาดความกว้างของ content ปรับปุ่ม
    แก้ขนาดความกว้างของ content ปรับปุ่ม

--- frontend/components/customer/cart/orders.jsx
+++ frontend/components/customer/cart/orders.jsx
@@ -111,60 +111,60 @@ const orders = () => {
 
   return (
     <form className="container my-5" onSubmit={handleSubmmit}>
-      <div className="row bg-light p-3 border rounded mb-4">
-        <h2 className="mb-2 text-center">ชำระเงิน</h2>
-        <hr className="my-4 border-secondary"/>
-        <h3>ที่อยู่ที่ใช้ในการจัดส่ง</h3>
-        <p>
-          <strong>
-            {address.f_name} {address.l_name} | โทร: 0{address.phone_number}
-          </strong>
-          <br />
-          {address.houesNo} ตำบล {address.tambon_nameTH} อำเภอ {address.amphure_nameTH} จังหวัด {address.province_nameTH} {address.zip_code}
-        </p>
-      </div>
-      <div className="row bg-light p-3 border rounded fw-bold">
-        <div className="col-12 col-md-6 d-none d-md-block text-secondary">รายการสินค้าที่สั่งซื้อ</div>
-        <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">ราคาต่อชุด</div>
-        <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">จำนวน</div>
-        <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">ราคารวม</div>
-      </div>
-      {productCart.length > 0 ? (
-        productCart.map((item) => (
-          <div className="row align-items-center p-3 border-bottom rounded bg-white" key={item.cart_product_id}>
-            <div className="col-12 col-md-6 d-flex align-items-center">
-              <img
-                src={`${API_URL_PICTURE}/${item.productpic_name}`}
-                alt={item.product_name}
-                className="img-fluid rounded"
-                style={{ width: '120px', height: '80px' }}
-              />
-              <span className="ms-3">{item.product_name}</span>
+      <div className="px-3 card-body">
+        <div className="row bg-light p-3 border rounded mb-4">
+          <h2 className="mb-2 text-center">ชำระเงิน</h2>
+          <hr className="my-4 border-secondary"/>
+          <h3>ที่อยู่ที่ใช้ในการจัดส่ง</h3>
+          <p>
+            <strong>
+              {address.f_name} {address.l_name} | โทร: 0{address.phone_number}
+            </strong>
+            <br />
+            {address.houesNo} ตำบล {address.tambon_nameTH} อำเภอ {address.amphure_nameTH} จังหวัด {address.province_nameTH} {address.zip_code}
+          </p>
+        </div>
+        <div className="row bg-light p-3 border rounded fw-bold">
+          <div className="col-12 col-md-6 d-none d-md-block text-secondary">รายการสินค้าที่สั่งซื้อ</div>
+          <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">ราคาต่อชุด</div>
+          <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">จำนวน</div>
+          <div className="col-4 col-md-2 text-center d-none d-md-block text-secondary">ราคารวม</div>
+        </div>
+        {productCart.length > 0 ? (
+          productCart.map((item) => (
+            <div className="row align-items-center p-3 border-bottom rounded bg-white" key={item.cart_product_id}>
+              <div className="col-12 col-md-6 d-flex align-items-center">
+                <img
+                  src={`${API_URL_PICTURE}/${item.productpic_name}`}
+                  alt={item.product_name}
+                  className="img-fluid rounded"
+                  style={{ width: '120px', height: '80px' }}
+                />
+                <span className="ms-3">{item.product_name}</span>
+              </div>
+              <div className="col-4 col-md-2 text-center">
+                <span className="text-secondary d-block d-md-none">ราคาต่อชุด</span>
+                <div>{numberGrouping(item.selling_price_per_quantity)} ฿</div>
+              </div>
+              <div className="col-4 col-md-2 text-center">
+                <span className="text-secondary d-block d-md-none">จำนวน</span>
+                <div>{item.quantity}</div>
+              </div>
+              <div className="col-4 col-md-2 text-center">
+                <span className="text-secondary d-block d-md-none">ราคารวม</span>
+                <div>{numberGrouping(item.selling_price_per_quantity * item.quantity)} ฿</div>
+              </div>
             </div>
-            <div className="col-4 col-md-2 text-center">
-              <span className="text-secondary d-block d-md-none">ราคาต่อชุด</span>
-              <div>{numberGrouping(item.selling_price_per_quantity)} ฿</div>
-            </div>
-            <div className="col-4 col-md-2 text-center">
-              <span className="text-secondary d-block d-md-none">จำนวน</span>
-              <div>{item.quantity}</div>
-            </div>
-            <div className="col-4 col-md-2 text-center">
-              <span className="text-secondary d-block d-md-none">ราคารวม</span>
-              <div>{numberGrouping(item.selling_price_per_quantity * item.quantity)} ฿</div>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p className="row bg-white p-3 justify-content-center align-items-center">ไม่มีสินค้าในตะกร้า</p>
-      )}
-      <div className="row bg-light p-3 border rounded text-end fw-bold">
-        <h4>รวมการสั่งซื้อ: {numberGrouping(totalPrice)} ฿</h4>
-        {/* <h4>ค่าจัดส่ง: {numberGrouping(totalWeight)} ฿</h4> */}
-        <h4>ยอดรวมทั้งหมด: {numberGrouping(totalPrice)} ฿</h4>
-      </div>
-      <div className="d-flex flex-column flex-md-row justify-content-end align-items-center mt-4">
-        <button type='submit' className="btn btn-success btn-lg">ชำระเงิน</button>
+          ))
+        ) : (
+          <p className="row bg-white p-3 justify-content-center align-items-center">ไม่มีสินค้าในตะกร้า</p>
+        )}
+        <div className="row bg-light p-3 border rounded text-end fw-bold">
+          <h4>ยอดรวมทั้งหมด: {numberGrouping(totalPrice)} ฿</h4>
+        </div>
+        <div className="d-flex flex-row-reverse bd-highlight mt-4">
+          <button type='submit' className="btn btn-success btn-lg">ชำระเงิน</button>
+        </div>
       </div>
     </form>
   );
