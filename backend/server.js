c6c26b01f40caa53cbf6aace4777c44870be5a30
EarthY.Y <supanat5947@gmail.com>
2024-12-03 16:05:14 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.use(session ({
   }
 }))
 
-app.listen(process.env.APP_PORT || 5000, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running `);
 })
 app.use('/picture', express.static(path.join(__dirname, 'picture')));
