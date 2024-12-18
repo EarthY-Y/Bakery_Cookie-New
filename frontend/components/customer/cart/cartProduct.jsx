11475711b6b23482be3a9461e56947909401a66b
EarthY.Y <supanat5947@gmail.com>
2024-12-18 16:14:29 +0700

    fix bug
    - หน้า cart รูปไม่ขึ้น

--- frontend/components/customer/cart/cartProduct.jsx
+++ frontend/components/customer/cart/cartProduct.jsx
@@ -99,7 +99,7 @@ const cartProduct = () => {
               <div className="row align-items-center pb-3 pt-3 border-bottom rounded bg-white" key={item.cart_product_id}>
                 <div className="col-12 col-md-8 d-flex align-items-center">
                   <img
-                    src={`${API_URL_PICTURE}/${item.productpic_name}`}
+                    src={`${API_URL_PICTURE} + ${item.productpic_name}`}
                     alt={item.product_name}
                     className="img-fluid rounded"
                     style={{ width: '120px', height: '80px' }}
