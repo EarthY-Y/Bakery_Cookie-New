b7f45903b5740afe1eae4b1eebb7bac0b3316eca
EarthY.Y <supanat5947@gmail.com>
2024-12-18 15:25:09 +0700

    Update categoryProductList.jsx

--- frontend/components/customer/product/categoryProductList.jsx
+++ frontend/components/customer/product/categoryProductList.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { listProductCategoryService } from '../../../API/customer/productService';
-const API_URL_PICTURE = import.meta.env.VITE_API_Port_PICTURE_PRODUCT
 import LoadingPopup from '../../untils/popUp/loading';
+const API_URL_PICTURE = import.meta.env.VITE_API_Port_PICTURE_PRODUCT
+
 const categoryProductList = () => {
   const [productsCategory, setProductsCategory] = useState([]);
   const {id} = useParams()
