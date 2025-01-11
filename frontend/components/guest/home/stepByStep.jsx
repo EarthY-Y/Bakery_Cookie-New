3a812edb86afd28805489c8453164d5b02ca5530
EarthY.Y <supanat5947@gmail.com>
2025-01-11 18:38:38 +0700

    เเก้รูปบน เว็บจริงไม่ขึ้น

--- frontend/components/guest/home/stepByStep.jsx
+++ frontend/components/guest/home/stepByStep.jsx
@@ -1,35 +1,37 @@
-const CategoryProductList = () => {
-  const steps = [
-    {
-      title: "1) เลือกสินค้าที่ต้องการสินค้าที่ต้องการ",
-      imgSrc: "../../../public/stepBystep/step1.png",
-    },
-    {
-      title: "2) เพิ่มจำนวนสินค้าตามที่ต้องการจากนั้นกด เพิ่มไปยังตะกร้าสินค้า",
-      imgSrc: "../../../public/stepBystep/step2.png",
-    },
-    {
-      title: "3) กดที่ไอคอน ตะกร้าสินค้า เพื่อเข้าสู่หน้าตะกร้าสินค้า",
-      imgSrc: "../../../public/stepBystep/step2.5.png",
-    },
-    {
-      title: "4) กดปุ่ม สั่งซื้อ เพื่อสรุปยอดการสั่งซื้อ",
-      imgSrc: "../../../public/stepBystep/step3.png",
-    },
-    {
-      title: "5) ระบบจะเเจ้งเตือนกรุณาอ่านคำเตือนก่อนกด ยืนยัน เพื่อให้เเน่ใจว่าท่านยอมรับกับเงื่อนไขในการสั่งซื้อได้",
-      imgSrc: "../../../public/stepBystep/step4.png",
-    },
-    {
-      title: "6) กรุณาเพิ่มที่อยู่ก่อนสั่งซื้อ หากมีที่อยู่มากกว่า 1 ที่ ระบบจะให้เลือกที่อยู่ในการจัดส่ง",
-      imgSrc: "../../../public/stepBystep/step5.png",
-    },
-    {
-      title: "7) อัปโหลดสลิปการชำระเงินตามยอดที่ระบุ จากนั้นอัปโหลดสลิปการชำระเงินเพื่อส่งให้ Admin ตรวจสอบ จากนั้นกด ชำระเงิน",
-      imgSrc: "../../../public/stepBystep/step6.png",
-    },
-  ];
+const API_URL_PICTURE = import.meta.env.VITE_API_Port_PICTURE_MANUAL
 
+const steps = [
+  {
+    title: "1) เลือกสินค้าที่ต้องการสินค้าที่ต้องการ",
+    imgName: "step1.png",
+  },
+  {
+    title: "2) เพิ่มจำนวนสินค้าตามที่ต้องการ จากนั้นกด เพิ่มไปยังตะกร้าสินค้า",
+    imgName: "step2.png",
+  },
+  {
+    title: "3) กดที่ตะกร้าสินค้า เพื่อเข้าสู่หน้าตะกร้าสินค้า",
+    imgName: "step2.5.png",
+  },
+  {
+    title: "4) กดปุ่ม สั่งซื้อ เพื่อสรุปยอดการสั่งซื้อ",
+    imgName: "step3.png",
+  },
+  {
+    title: "5) ระบบจะเเจ้งเตือนกรุณาอ่านคำเตือนก่อนกดยืนยัน เพื่อให้เเน่ใจว่าท่านยอมรับกับเงื่อนไขในการสั่งซื้อได้",
+    imgName: "step4.png",
+  },
+  {
+    title: "6) กรุณาเพิ่มที่อยู่ก่อนสั่งซื้อ หากมีที่อยู่มากกว่า 1 ที่ ระบบจะให้เลือกที่อยู่ในการจัดส่ง",
+    imgName: "step5.png",
+  },
+  {
+    title: "7) อัปโหลดสลิปการชำระเงินตามยอดที่ระบุ จากนั้นอัปโหลดสลิปการชำระเงินเพื่อส่งให้ Admin ตรวจสอบ จากนั้นกด ชำระเงิน",
+    imgName: "step6.png",
+  },
+];
+
+const CategoryProductList = () => {
   return (
     <div className="container my-5">
       <div className="row">
@@ -39,11 +41,15 @@ const CategoryProductList = () => {
       </div>
       {steps.map((step, index) => (
         <div key={index} className="row align-items-center mb-5">
-          <div className="col-md-8 text-center">
-            <img src={step.imgSrc} alt={`ขั้นตอนที่ ${index + 1}`} className="img-fluid rounded shadow" />
+          <div className="col-md-6 text-center">
+            <img
+              src={`${API_URL_PICTURE}${step.imgName}`}
+              alt={`ขั้นตอนที่ ${index + 1}`}
+              className="img-fluid rounded shadow"
+            />
           </div>
-          <div className="col-md-4">
-            <h4 className="text-danger">{step.title}</h4>
+          <div className="col-md-6">
+            <h4 className="text-primary">{step.title}</h4>
           </div>
         </div>
       ))}
@@ -52,3 +58,4 @@ const CategoryProductList = () => {
 };
 
 export default CategoryProductList;
+
