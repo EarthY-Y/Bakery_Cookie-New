81cc30add1734ba59aa3f383d0eb807f59ceb02f
EarthY.Y <supanat5947@gmail.com>
2025-01-12 12:38:01 +0700

    เเก้

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 <script src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
-const API_URL_PICTURE = import.meta.env.VITE_API_Port_PICTURE_LOGO;
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode> //StrictMode ทำให้เกิดการ render สองครั้งในระหว่างการพัฒนา response ที่ได้เบิ้ลเป็นสองรอบ
