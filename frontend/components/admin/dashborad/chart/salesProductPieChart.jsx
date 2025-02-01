ef845112e5020ffdff83a260dfffd3b83b070225
EarthY.Y <supanat5947@gmail.com>
2025-02-01 09:37:08 +0700

    ปรับให้ Pie chart เเสดงผลเป็น %

--- frontend/components/admin/dashborad/chart/salesProductPieChart.jsx
+++ frontend/components/admin/dashborad/chart/salesProductPieChart.jsx
@@ -1,62 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { Line, Pie} from "react-chartjs-2";
+import { Line, Pie } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, ArcElement, Tooltip, Legend, Title, } from "chart.js";
 import LoadingPopup from "../../../untils/popUp/loading";
 
 ChartJS.register(CategoryScale, LinearScale, ArcElement, Tooltip, Legend, Title);
 
-const salesProductPieChart = ({ dataSales }) => {
+const SalesProductPieChart  = ({ dataSales }) => {
   const [isLoading, setIsLoading] = useState(false);
-  // Mapping เดือนจากตัวเลขเป็นชื่อภาษาไทย
-  const monthMapping = {
-    "01": "มกราคม",
-    "02": "กุมภาพันธ์",
-    "03": "มีนาคม",
-    "04": "เมษายน",
-    "05": "พฤษภาคม",
-    "06": "มิถุนายน",
-    "07": "กรกฎาคม",
-    "08": "สิงหาคม",
-    "09": "กันยายน",
-    "10": "ตุลาคม",
-    "11": "พฤศจิกายน",
-    "12": "ธันวาคม",
-  };
-  
+
   // เตรียมข้อมูลเพื่อสร้างกราฟ
-    // กรองข้อมูลที่มี total_quantity เป็น null ออก
-    const filteredDataSales = dataSales.filter((item) => item.total_quantity !== null);
+  // กรองข้อมูลที่มี percentage เป็น null ออก
+  const filteredDataSales = dataSales.filter((item) => item.percentage !== null);
 
-    // หากไม่มีข้อมูลให้แสดงข้อความ
-    if (filteredDataSales.length === 0) {
-      return <p className="text-center">ไม่มีข้อมูลสำหรับแสดงกราฟ</p>;
-    }
-  const labels = filteredDataSales.map((item) => item.product_name);
-  const totalQuantity = filteredDataSales.map((item) => item.total_quantity);
+  // หากไม่มีข้อมูลให้แสดงข้อความ
+  if (filteredDataSales.length === 0) {
+    return <p className="text-center">ไม่มีข้อมูลสำหรับแสดงกราฟ</p>;
+  }
+  const labels = filteredDataSales.map((item) => `${item.product_name} (${item.percentage}%)`);
+  const percentage = filteredDataSales.map((item) => item.percentage);
 
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'ขายไปทั้งสิ้น',
-    data: totalQuantity,
-    backgroundColor: [
-      "rgb(255, 99, 132)",
-      "rgb(54, 162, 235)",
-      "rgb(255, 205, 86)",
-      "rgb(75, 192, 192)",
-      "rgb(153, 102, 255)",
-    ],
-    hoverOffset: 3 //เวลา hover เเล้ว pie จะลอยขึ้น
-  }]
-};
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'ขายไปทั้งสิ้น',
+      data: percentage,
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(153, 102, 255)",
+      ],
+      hoverOffset: 3 //เวลา hover เเล้ว pie จะลอยขึ้น
+    }]
+  };
 
   const options = {
     responsive: true,
     plugins: {
+      //แสดง % ตอน hover โดยใช้ tooltip formatter
+      tooltip: {
+        callbacks: { 
+          label: (tooltipItem) => {
+            let value = tooltipItem.raw; // ค่าที่ส่งมา
+            return ` ${value}%`; // แสดงเป็นเปอร์เซ็นต์
+          }
+        }
+      },
       legend: {
         position: "top",
-        labels:{
-          font:{
+        labels: {
+          font: {
             size: 20
           }
         }
@@ -64,7 +58,7 @@ const data = {
       title: {
         display: true,
         text: "ยอดขายย้อนหลัง 30 วัน",
-        font:{
+        font: {
           size: 20,
         },
       },
@@ -82,7 +76,7 @@ const data = {
         <LoadingPopup isLoading={isLoading} />
         {isLoading && <div className="modal-backdrop fade show"></div>}
       </div>
-      <div className="d-none d-md-block text-center" style={{ width: "50%", height: "50%", display: "flex",justifyContent: "center",alignItems: "center"}}>
+      <div className="d-none d-md-block text-center" style={{ width: "50%", height: "50%", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Pie data={data} options={options} />
       </div>
       <div className="d-block d-md-none text-center">
@@ -92,4 +86,4 @@ const data = {
   );
 };
 
-export default salesProductPieChart;
+export default SalesProductPieChart ;
