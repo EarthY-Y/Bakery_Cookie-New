208ae4e028f83f3ff048d1b07485b52d08193335
kieee1246 <kieee1246@gmail.com>
2025-03-19 20:51:41 +0700

    edit
    edit name

--- frontend/components/guest/layOut/footer/footer.jsx
+++ frontend/components/guest/layOut/footer/footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
     <footer className="text-white py-5 d-flex align-items-end flex-column" style={{ backgroundColor: '#C40C0C'}}>
       <div className="container col-12 text-start mt-auto p-2 d-none d-lg-block">
         <h5>About</h5>
-        <p>Bakery Cookie&New ร้านเบเกอรี่เล็กๆ ใจกลางรามอินทรา 101 โดดเด่นด้วยขนมอบสดใหม่ที่ทำตามออร์เดอร์ทุกชิ้น พี่นุชไม่ว่าจะเป็นเค้ก ขนมปัง หรือคุกกี้ รับประกันความสดใหม่และรสชาติที่ใส่ใจในทุกขั้นตอน 
+        <p>Bakery Cookie&New ร้านเบเกอรี่เล็กๆ ใจกลางรามอินทรา 101 โดดเด่นด้วยขนมอบสดใหม่ที่ทำตามออร์เดอร์ทุกชิ้น ไม่ว่าจะเป็นเค้ก ขนมปัง หรือคุกกี้ รับประกันความสดใหม่และรสชาติที่ใส่ใจในทุกขั้นตอน 
           พร้อมเสิร์ฟความอร่อยให้กับทุกโอกาสพิเศษของคุณ!</p>
         <h5>Contact</h5>
         <p>Tel: 0891175751 <br />Line official account: @421qvgwl</p>
